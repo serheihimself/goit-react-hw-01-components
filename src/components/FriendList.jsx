9b0ea7680d17895e15fb2e979fd84fd6b1dbb9e6
serheihimself <sergm080@gmail.com>
2023-02-19 22:31:42 +0200

    task 3 final commit

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -25,10 +25,14 @@ function FriendList({ friends }) {
   );
 }
 FriendList.propTypes = {
-  name: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export default FriendList;
